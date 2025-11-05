03380e528ebc1fa519135f6e18538caa2ef9e7b7
pranav-k-jha <pranav.jha@mail.concordia.ca>
2025-11-04 19:06:33 -0500

    Updates

--- assets/About.jKNyTa5R.js
+++ assets/About.jKNyTa5R.js
@@ -1,4 +1,4 @@
-import{c as a,u as d,j as e,m as i,B as o,S as x}from"./main.ByhLHCw5.js";import{C as m}from"./clock.DG6Nm0gH.js";/**
+import{c as a,u as d,j as e,m as i,B as o,S as x}from"./main.DXIoP7iu.js";import{C as m}from"./clock.DZIYPW5p.js";/**
  * @license lucide-react v0.548.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/clock.DZIYPW5p.js
+++ assets/clock.DZIYPW5p.js
@@ -1,4 +1,4 @@
-import{c}from"./main.ByhLHCw5.js";/**
+import{c}from"./main.DXIoP7iu.js";/**
  * @license lucide-react v0.548.0 - ISC
  *
  * This source code is licensed under the ISC license.

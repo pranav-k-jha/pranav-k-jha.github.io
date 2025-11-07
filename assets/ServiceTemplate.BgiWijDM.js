f361f0f62b653de47471a9a29e701388ad29fc2c
pranav-k-jha <pranav.jha@mail.concordia.ca>
2025-11-06 20:59:21 -0500

    Updates

--- assets/ServiceTemplate.BgiWijDM.js
+++ assets/ServiceTemplate.BgiWijDM.js
@@ -1,4 +1,4 @@
-import{c as p,r as d,a as S,j as e,m as t,Z as L,f as m,b as x,L as h}from"./main.DngReQY5.js";/**
+import{c as p,r as d,a as S,j as e,m as t,Z as L,f as m,b as x,L as h}from"./main.CjFdi4XZ.js";/**
  * @license lucide-react v0.548.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/clock.CBfc163w.js
+++ assets/clock.CBfc163w.js
@@ -1,4 +1,4 @@
-import{c}from"./main.DngReQY5.js";/**
+import{c}from"./main.CjFdi4XZ.js";/**
  * @license lucide-react v0.548.0 - ISC
  *
  * This source code is licensed under the ISC license.

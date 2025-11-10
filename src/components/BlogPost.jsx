b49bfef71c0dcf1d2d7af6b6e259f93d9192eedc
pranav-k-jha <pranav.jha@mail.concordia.ca>
2025-11-09 23:16:40 -0500

    upated blog page and posts

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -166,7 +166,7 @@ export default function BlogPost() {
                   ),
                   h3: (props) => (
                     <h3
-                      className="text-lg md:text-xl font-semibold mt-6 mb-2"
+                      className="text-sm md:text-xl font-semibold mt-6 mb-2"
                       {...props}
                     />
                   ),

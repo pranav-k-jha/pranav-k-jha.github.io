03f210f4fe6b7df8f7f032d03a436a87ce07041e
pranav-k-jha <pranav.jha@mail.concordia.ca>
2025-11-24 12:31:15 -0500

    updated navbar

--- src/pages/PublicationsPage.jsx
+++ src/pages/PublicationsPage.jsx
@@ -62,26 +62,6 @@ const PublicationsPage = () => {
           title: {
             title: {
               value:
-                "A Traffic Conflict Resolution Model using Edge Computing for Urban Arterial Networks",
-            },
-          },
-          "publication-date": { year: { value: "2025" } },
-          "journal-title": { value: "MDPI Applied Sciences" },
-          "journal-subtitle": { value: "Submitted" },
-          "work-type": { value: "JOURNAL_ARTICLE" },
-          type: "JOURNAL_ARTICLE",
-          url: { value: "https://www.mdpi.com/journal/applsci" },
-          manual: true,
-          "publication-status": "submitted",
-        },
-      ],
-    },
-    {
-      "work-summary": [
-        {
-          title: {
-            title: {
-              value:
                 "Generative Models for Synthetic Transit Trip Data: A Comparative Study of CTGAN and TVAE for Travel Behavior Analysis",
             },
           },
